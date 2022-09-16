15133a57ae4cb39d146f73d76a0864c6eec5e8eb
Liming Cai <34404264+lmcai@users.noreply.github.com>
2022-09-15 20:58:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 driver = webdriver.Chrome("/Applications/chromedriver”)
-driver.get("http://www.cvh.ac.cn/spms/list.php?taxonName=&family=Lamiaceae&genus=&country=&county=&locality=&altitude=&recordedBy=&recordNumber=70316&year=&collectionCode=&identifiedBy=&dateIdentified=“)
-soup = BeautifulSoup(driver.page_source)
-result=soup.find_all('tr',attrs={'class':'spms-row'})
-result[0].attrs['data-collection-id']
-driver.get("http://www.cvh.ac.cn/spms/info.php?id="+ result[0].attrs['data-collection-id’])
-soup = BeautifulSoup(driver.page_source)
-sp_nam=soup.find_all('td',attrs={'id':'formattedName’})[0].contents
+files=open('HUH_rec.csv').readlines()
+
+def scraper(family,collection_ID):
+	driver.get("http://www.cvh.ac.cn/spms/list.php?taxonName=&family="+family+"&genus=&country=&county=&locality=&altitude=&recordedBy=&recordNumber="+collection_ID+"&year=&collectionCode=&identifiedBy=&dateIdentified=“)
+	soup = BeautifulSoup(driver.page_source)
+	result=soup.find_all('tr',attrs={'class':'spms-row'})
+	result[0].attrs['data-collection-id']
+	driver.get("http://www.cvh.ac.cn/spms/info.php?id="+ result[0].attrs['data-collection-id’])
+	soup = BeautifulSoup(driver.page_source)
+	sp_nam=soup.find_all('td',attrs={'id':'formattedName’})[0].contents
+
+for line in files:
+	scraper(line)
